4e0185ef8f54d4fb6ace48e703aa84ace7641f16
el jadakrino <Jadakra@users.noreply.github.com>
2019-12-07 14:07:57 -0600

    Commit inicial
    jhfkahskfjh132513pipo

---
+++ GITClass/Assets/Scripts/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed = 1;
+    public float speedRot = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Debug.Log(Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Debug.Log(Time.deltaTime);
+        transform.Translate(Vector3.right * speed * Input.GetAxis("Vertical")* Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.forward * speedRot * Input.GetAxis("Horizontal") * Time.deltaTime);
+        //this.gameObject.transform.Rotate(Quaternion.EulerRotation(30, 0, 0));
+    }
+}
---
+++ GITClass/Assets/Scripts/CityGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject carPrefab;
+    public GameObject[] TodasLasPistas = new GameObject[12];
+   
+    void Start()
+    {
+        Instantiate(carPrefab, Vector3.zero, Quaternion.Euler(-90f, 0f, 0f));
+        CreateCity();
+
+    }
+
+    private void CreateCity()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            int number = UnityEngine.Random.Range(0, 12);
+            Instantiate(TodasLasPistas[number], new Vector3(0,0, 200 * i), Quaternion.Euler(0, 0f, 0f));
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
