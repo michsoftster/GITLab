b69297038c020b6efc6a3150d3bdf5a25ebd8d7f
Luis Constantino Mota <lcmota94@gmail.com>
2019-12-07 13:58:45 -0600

    nuevo avance final

---
+++ GITClass/Assets/scripts/CarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float speed;
+    public float speedRot;
+    void Update()
+    {
+        transform.Translate(Vector3.right *
+                                    speed *
+                                    Input.GetAxis("Vertical") *
+                                    Time.deltaTime);
+        transform.Rotate(Vector3.forward *
+                                speedRot *
+                                Input.GetAxis("Horizontal") *
+                                Time.deltaTime);
+    }
+}
---
+++ GITClass/Assets/scripts/CityGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CityGenerator : MonoBehaviour
+{
+    public GameObject carPrefab;
+   public GameObject [] calles = new GameObject [12];
+    void Start()
+    {
+        Instantiate(carPrefab,
+                    Vector3.zero,
+                    Quaternion.Euler(-90f,
+                                        0f,
+                                        0f));
+                                     
+        CreateCity(
+          
+        );
+    }
+
+    // Update is called once per frame
+    void CreateCity()
+    {
+                  
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0,LoadSceneMode.Single);
+    }
+    }
+}
