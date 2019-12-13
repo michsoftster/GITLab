75a909da44ca4fe5ada996c5f772611d353fd86f
Iván González <clowncrown10@gmail.com>
2019-12-12 23:19:47 -0600

    cambio de pista con z

--- GITClass/Assets/Script/CityGenerator.cs
+++ GITClass/Assets/Script/CityGenerator.cs
@@ -6,10 +6,10 @@ using UnityEngine.SceneManagement;
 public class CityGenerator : MonoBehaviour
 {
 
-    public GameObject[] carreteras = new GameObject[11];
+    public GameObject[] carreteras = new GameObject[10];
     void Start()
     {
-        for (int i = 0; i < 11; i++)
+        for (int i = 0; i < 10; i++)
         {
             Instantiate(carreteras[Random.Range(0, 10)],Vector3.forward*(i*200),Quaternion.Euler(0,0,0));
         }
