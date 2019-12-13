c15ebaf0249054c8c7d7d5c9c79e1b7a940831ff
Iván González <clowncrown10@gmail.com>
2019-12-12 22:59:04 -0600

    cambio de pista con z

---
+++ GITClass/Assets/Script/LoadSceneCity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneCity : MonoBehaviour
+{
+    public GameObject carro;
+    public GameObject origin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(carro);
+        DontDestroyOnLoad(origin);
+        SceneManager.LoadScene("scene1", LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown("z"))
+        {
+                
+            SceneManager.LoadScene("scene2", LoadSceneMode.Single);
+        
+        }
+    }
+}
