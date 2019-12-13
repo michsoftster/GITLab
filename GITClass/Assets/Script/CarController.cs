a99a34de72f2f6a1b853c68f4af49024b53370c7
Iván González <clowncrown10@gmail.com>
2019-12-12 20:28:26 -0600

    carro con movimiento en la pista

--- GITClass/Assets/Script/CarController.cs
+++ GITClass/Assets/Script/CarController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    
+    public float speed;
+    public float speedRot;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    
     // Update is called once per frame
+ 
     void Update()
     {
         
+        transform.Translate(Vector3.right *
+                                    speed *
+                                    Input.GetAxis("Vertical") *
+                                    Time.deltaTime);
+        transform.Rotate(Vector3.up *
+                                speedRot *
+                                Input.GetAxis("Horizontal") *
+                                Time.deltaTime,Space.World);
     }
 }
