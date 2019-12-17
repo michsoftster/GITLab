9da56370198de60e3288aaeffff821526f54c08c
mabcbot <ing.manuelbustamante@gmail.com>
2019-12-16 21:18:26 -0600

    Practica 1
    Cambios necesarios para la realizacion de la practica 1 y correccciones minimas a los prefabs de carretera

--- GITClass/Assets/scripts/CityGenerator.cs
+++ GITClass/Assets/scripts/CityGenerator.cs
@@ -2,33 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+
 public class CityGenerator : MonoBehaviour
 {
     public GameObject carPrefab;
-   public GameObject [] calles = new GameObject [12];
+    public GameObject[] calles = new GameObject [12];
     void Start()
     {
-        Instantiate(carPrefab,
-                    Vector3.zero,
-                    Quaternion.Euler(-90f,
-                                        0f,
-                                        0f));
-                                     
-        CreateCity(
-          
-        );
+        Instantiate(carPrefab, Vector3.zero, Quaternion.Euler(-90f, 0f, -90f));
+
+        CreateCity();
     }
 
-    // Update is called once per frame
     void CreateCity()
     {
-                  
+        System.Random random = new System.Random();
+        int index;
+        for (int i = 0; i < 12; i++)
+        {
+            index = random.Next(0, 12);
+            print(index);
+            Instantiate(calles[index], new Vector3(0f, 0f, (i * 200f)), Quaternion.Euler(0f, 0f, 0f));
+        }
     }
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(0,LoadSceneMode.Single);
-    }
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+            CreateCity();
+        }
     }
 }
